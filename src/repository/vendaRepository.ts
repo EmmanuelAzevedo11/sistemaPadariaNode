6328114d1817c0745c59b6b842f1667073b27c45
Emmanuel Azevedo <emmanuelforcinetti123@gmail.com>
2026-04-23 11:06:34 -0300

    Finalizando as rotas de vendas

---
+++ src/repository/vendaRepository.ts
@@ -0,0 +1,104 @@
+
+import { prisma } from "../lib/prisma";
+
+export const buscarVendas = async () => {
+    const listaVendas = await prisma.venda.findMany();
+
+    if(listaVendas.length === 0 ){
+        throw new Error("Não existe vendas ainda");
+    }
+
+    return listaVendas;
+}
+
+export const buscarVendaPorId = async (id: number) => {
+    const venda = await prisma.venda.findUnique({
+        where: {
+            id: Number(id)
+        }
+    });
+
+    if(!venda){
+        throw new Error("Não existe essa venda");
+    }
+
+    return venda;
+}
+
+export const criarVenda = async (valorTotalBruto: number,  valorTotalLiquido: number,  dataHoraVenda: Date, vendedorId: number, 
+    clienteId: number, formaPagamentoId: number , descontoAplicado?: number, cpfNotaFiscal?: string,) => {
+        
+        if(!valorTotalBruto || !valorTotalLiquido || !dataHoraVenda || !vendedorId || !clienteId || !formaPagamentoId){
+            throw new Error("Dados inválidos para realizar a venda");
+        }
+
+        const vendaCriada = await prisma.venda.create({
+            data: {
+                valorTotalBruto: valorTotalBruto,
+                valorTotalLiquido: valorTotalLiquido,
+                dataHoraVenda: dataHoraVenda,
+                vendedor: {
+                    connect: {id: Number(vendedorId)}
+                },
+                cliente: {
+                    connect: {id: Number(clienteId)}
+                },
+                formaPagamento: {
+                    connect: {id: Number(formaPagamentoId)}
+                },
+                descontoAplicado: descontoAplicado ?? undefined,
+                cpfNotaFiscal: cpfNotaFiscal ?? null,
+            }
+        });
+
+        return vendaCriada;
+};
+
+export const updateVendas = async (id: number, dados: any) => {
+    const vendaEncontrada = await buscarVendaPorId(id);
+
+    if(!vendaEncontrada){
+        throw new Error("Venda não encontrada");
+    }
+
+    const { valorTotalBruto , valorTotalLiquido  ,dataHoraVenda, vendedorId ,
+    clienteId ,formaPagamentoId, descontoAplicado = 0 , cpfNotaFiscal = undefined } = dados;
+
+
+    if(!valorTotalBruto || !valorTotalLiquido || !dataHoraVenda || !vendedorId || !clienteId || !formaPagamentoId){
+        throw new Error("Dados inválidos para realizar a venda");
+    }
+
+    const vendaAtualizada = await prisma.venda.update({
+        where: { id: Number(id) },
+        data: {
+            valorTotalBruto: valorTotalBruto,
+            valorTotalLiquido: valorTotalLiquido,
+            dataHoraVenda: dataHoraVenda,
+            vendedorId: vendedorId,
+            clienteId: clienteId,
+            formaPagamentoId: formaPagamentoId,
+            descontoAplicado: descontoAplicado,
+            cpfNotaFiscal: cpfNotaFiscal
+        }
+    });
+
+    return vendaAtualizada;
+};
+
+export const deleteVenda = async(id:number) => {
+    const vendaExiste = await buscarVendaPorId(id);
+
+    if(!vendaExiste){
+        throw new Error("Essa venda não existe");   
+    }
+
+    await prisma.venda.delete({
+        where: {
+            id: Number(id)
+        }
+    });
+
+    return true
+
+}
--- src/routes/vendasRoutes.ts
+++ src/routes/vendasRoutes.ts
@@ -1,5 +1,94 @@
 import { Router } from "express";
+import { buscarVendaPorId, buscarVendas, criarVenda, deleteVenda, updateVendas } from "../repository/vendaRepository";
 
 const route = Router();
+
+route.get('/venda', async (req, res) => {
+    try {
+        const listaVendas = await buscarVendas(); 
+
+        return res.status(200).json({
+            message: "Lista de vendas encontrada com sucesso",
+            listaVendas: listaVendas
+        });
+
+    } catch (error: any) {
+        return res.status(400).json({
+            message: error.message
+        });
+    }
+});
+
+route.get('/venda/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const venda = await buscarVendaPorId(Number(id));
+
+        return res.status(200).json({
+            message: "Venda encontrada com sucesso",
+            venda: venda
+        });
+
+    } catch (error: any) {
+        return res.status(400).json({
+            message: error.message
+        });
+    }
+});
+
+route.post('/venda', async(req, res) => {
+    try {
+        const { valorTotalBruto , valorTotalLiquido  ,dataHoraVenda, vendedorId ,
+            clienteId ,formaPagamentoId, descontoAplicado = 0 , cpfNotaFiscal = undefined } = req.body;
+
+        const vendaCriada = await criarVenda(valorTotalBruto,valorTotalLiquido,dataHoraVenda,vendedorId,clienteId,
+            formaPagamentoId,descontoAplicado, cpfNotaFiscal
+        );
+
+        return res.status(201).json({
+            message: "Venda criada com sucesso",
+            vendaCriada: vendaCriada
+        });
+
+    } catch (error: any) {
+        return res.status(422).json({
+            message: error.message
+        });
+    }
+});
+
+route.put('/venda/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const vendaAtualizada = await updateVendas(Number(id), req.body);
+
+        return res.status(200).json({
+            message: "Venda atualizada com sucesso",
+            vendaAtualizada: vendaAtualizada
+        });
+
+    } catch (error: any) {
+        return res.status(422).json({
+            message: error.message, 
+        });
+    }
+
+});
+
+route.delete('/venda/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const retorno = await deleteVenda(Number(id));
+
+        return res.status(200).json({
+            message: "Venda excluida com sucesso",
+            retorno: retorno
+        });
+    } catch (error: any) {
+        return res.status(400).json({
+            message: error.message
+        });
+    }
+});
 
 export default route;
